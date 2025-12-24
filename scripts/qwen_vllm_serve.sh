70407ea5799bd76b148c62ef22ca464139601309
ohjoonhee <ohjoonhee@snu.ac.kr>
2025-12-24 20:53:31 +0900

    feat(hrbench): Add HRBench scripts and prompts

--- scripts/qwen_vllm_serve.sh
+++ scripts/qwen_vllm_serve.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-vllm serve Qwen/Qwen3-VL-8B-Thinking \
+vllm serve Qwen/Qwen3-VL-2B-Instruct \
     --enable-auto-tool-choice \
     --tool-call-parser hermes \
     --gpu-memory-utilization 0.95 \
-    --enforce-eager \
-    --max-model-len 32768 \
+    --max-model-len 131072 \
     # --quantization fp8
     # --reasoning-parser deepseek_r1 \
